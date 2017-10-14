f8a020bc0602f4a9cc8a5a260dc5664a2b620e93
student <s528805@nw39593.nwmissouri.edu>
2017-10-14 16:20:15 -0500

    Initial Commit

---
+++ The Missouri Birder's Apprentice/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  The Missouri Birder's Apprentice
+//
+//  Created by Laddipeerla,Karthik on 10/14/17.
+//  Copyright © 2017 Laddipeerla,Karthik. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ The Missouri Birder's Apprentice/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  The Missouri Birder's Apprentice
+//
+//  Created by Laddipeerla,Karthik on 10/14/17.
+//  Copyright © 2017 Laddipeerla,Karthik. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
